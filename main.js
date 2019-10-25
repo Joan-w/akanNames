4f39e70dc47c55f826bad055a05132a868ca764d
murathe <isaacsm07p@gmail.com>
2019-10-25 21:51:03 +0300

    declare variables

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-monday && male 
+var century = parseInt(get.elementtById(century));
+var year = parseInt(get.elementtById(year));
+var month = parseInt(get.elementtById(month));
+var day = parseInt(get.elementtById(day));
 
-console.log("Your name is " + result);
+var maleNames = ["", "", "", "", "", "", ""];
+var femaleNames = ["", "", "", "", "", "", ""];
+
+if () {
+    
+} else {
+    
+}
