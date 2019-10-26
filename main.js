0bb5ef5219c4d578299b1375ccadcb39f8b229e4
murathe <isaacsm07p@gmail.com>
2019-10-26 23:54:30 +0300

    Compile gender and day matching code

--- main.js
+++ main.js
@@ -7,5 +7,67 @@ var maleNames = ["", "", "", "", "", "", ""];
 var femaleNames = ["", "", "", "", "", "", ""];
 
 let readGender = () =>{
-    var gend = 
+    var gend = document.getElementsByName("gender");
+    if (gender[0].checked == true) {
+        var gender = "male";
+    } else if (gender[1].checked == true){
+        var gender = "female";   
+    } else{
+        alert("Please choose your gender");
+    }
+    switch(gender){
+        case gender = "Male":
+            switch (day){
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[0]+ "" + "Your Akan name is" + maleNames[0];
+                    break;
+                case (1 || -1):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[1]+ "" + "Your Akan name is" + maleNames[1];
+                    break;
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[2]+ "" + "Your Akan name is" + maleNames[2];
+                    break;
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[3]+ "" + "Your Akan name is" + maleNames[3];
+                    break;
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[4]+ "" + "Your Akan name is" + maleNames[4];
+                    break;
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[5]+ "" + "Your Akan name is" + maleNames[5];
+                    break;
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[6]+ "" + "Your Akan name is" + maleNames[6];
+                    break;
+                default:
+            }
+
+            break;
+        case gender = "Female":
+            switch (day){
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[0]+ "" + "Your Akan name is" + maleNames[0];
+                    break;
+                case (1 || -1):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[1]+ "" + "Your Akan name is" + maleNames[1];
+                    break;
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[2]+ "" + "Your Akan name is" + maleNames[2];
+                    break;
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[3]+ "" + "Your Akan name is" + maleNames[3];
+                    break;
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[4]+ "" + "Your Akan name is" + maleNames[4];
+                    break;
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[5]+ "" + "Your Akan name is" + maleNames[5];
+                    break;
+                case (0 || -0):
+                    document.getElementById("result").innerHTML = "The day is on a " + days_array[6]+ "" + "Your Akan name is" + maleNames[6];
+                    break;
+                default:
+    }
+    break;
+    default:
 }
